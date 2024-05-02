3924a5cb0d288bc419387a5456e36e5a3a3cfba4
“Gemmacentr” <gemma.centrone@student.unisi.it>
2024-05-02 18:22:57 +0200

    c

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: "https://github.com/Gemmacentr/vueApp/",
+  plugins: [],
+  resolve: {
+    alias: [],
+  },
 });
