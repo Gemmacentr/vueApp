02dd6d23847fc51164d2b1307a009cde694ce6cf
“Gemmacentr” <gemma.centrone@student.unisi.it>
2024-05-03 09:30:43 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: "https://github.com/Gemmacentr/vueApp/",
   plugins: [vue()],
-  resolve: {
-    alias: [],
-  },
-});
-// vue.config.js
-module.exports = {
-  devServer: {
+  server: {
     proxy: {
       "/assets": {
         target: "https://github.com/Gemmacentr/vueApp",
         changeOrigin: true,
-        pathRewrite: {
-          "^/assets": "/assets",
-        },
+        rewrite: (path) => path.replace(/^\/assets/, ""),
       },
     },
   },
-};
+});
